748a4747e4d85080acc9bbbf058f0866dcda8cd7
Pratchaya Jutavichit <eazterohfinz@gmail.com>
2017-12-18 03:21:06 +0700

    Edit contacts file

--- contacts/index.js
+++ contacts/index.js
@@ -2,68 +2,93 @@ const express = require('express')
 const router = express.Router()
 var lists = [
     {
-        "ID" : "0",
+        "ID": "0",
         "Firstname": "Piyapong",
         "Lastname": "Paikamnam",
         "URL": "https://www.facebook.com/piyaphong.paikamnam",
-        "Phone" : "082-5237431",
-        "Note" : ""
+        "Phone": "082-5237431",
+        "Note": ""
     },
     {
-        "ID" : "1",
+        "ID": "1",
         "Firstname": "Nattaphurin",
         "Lastname": "Vichit",
         "URL": "https://www.facebook.com/profile.php?id=100003002294477",
-        "Phone" : "098-9367670",
-        "Note" : ""
+        "Phone": "098-9367670",
+        "Note": ""
     },
     {
-        "ID" : "2",
+        "ID": "2",
         "Firstname": "Pongsapak",
         "Lastname": "Rattanaaudomsuk",
         "URL": "https://www.facebook.com/MPongsapak",
-        "Phone" : "081-1558446",
-        "Note" : ""
+        "Phone": "081-1558446",
+        "Note": ""
     },
     {
-        "ID" : "3",
+        "ID": "3",
         "Firstname": "Pongsiri",
         "Lastname": "Suasad",
         "URL": "https://www.facebook.com/akkarapol.suasat",
-        "Phone" : "061-3269130",
-        "Note" : ""
+        "Phone": "061-3269130",
+        "Note": ""
     },
     {
-        "ID" : "4",
+        "ID": "4",
         "Firstname": "Pratchaya",
         "Lastname": "Jutavichit",
         "URL": "https://www.facebook.com/Bterpchy",
-        "Phone" : "083-1609631",
-        "Note" : ""
+        "Phone": "083-1609631",
+        "Note": ""
     }
-    ];
-    indAllofList = function() {
-        return lists;
-    };
-    findByFirstname = function (name) {
-        for (var i = 0; i < lists.length; i++) {
-            if (lists[i].Firstname ==name) return lists[i];
-        }
-    };
-    findByID = function (ID) {
-        for (var i = 0; i <lists.length; i++) {
-            if (lists[i].ID == ID) return lists[i];
-        }
-    };
-    router.get('/list/name/:name', (req,res) =>{
-        var name = req.params.name;
-        res.json(findByFirstname(name));
-    })
-    router.get('/list/id/:id', (req,res) =>{
-        var id = req.params.id;
-        res.json(findByID(id));
-    })
-    router.get('/list', function (req, res) {
-        res.json(findAllofList());
-    });
-    module.exports = router
+];
+findAllofList = function () {
+    return lists;
+};
+findByID = function (id) {
+    for (var i = 0; i < lists.length; i++) {
+        if (lists[i].ID == id) return lists[i];
+    }
+};
+findByFirstname = function (name) {
+    for (var i = 0; i < lists.length; i++) {
+        if (lists[i].Firstname == name) return lists[i];
+    }
+};
+// ค้นหาทั้งหมด
+router.get('/list', function (req, res) {
+    res.json(findAllofList());
+})
+// ค้นหาด้วยชื่อ
+router.get('/list/name/:name', (req, res) => {
+    var name = req.params.name;
+    res.json(findByFirstname(name));
+})
+// ค้นหาด้วยรหัส
+router.get('/list/id/:id', (req, res) => {
+    var id = req.params.id;
+    res.json(findByID(id));
+})
+//เพิ่มข้อมูล
+router.post('/list/add', (req, res) => {
+    var newlist = req.body
+    newlist.ID = lists.length
+    lists.push(newlist)
+    res.status(201).json('Add new list ID: ' + newlist.ID + ' FIRSTNAME: ' + newlist.Firstname + ' Complete')
+})
+//อัปเดตข้อมูลที่มีอยู่
+router.put('/list/update/:id', (req, res) => {
+    var uplist = req.body
+    var id = req.params.id
+    lists[id] = uplist
+    res.status(200).json('Update Complete!')
+})
+//ลบข้อมูล
+router.delete('/list/delete/:id', (req, res) => {
+    var id = req.params.id
+    lists.splice(id, 1)
+    res.status(201).json('Delete Complete!')
+
+})
+
+module.exports = router
