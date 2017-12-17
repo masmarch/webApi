6a70c48970e7e2d0184d72cd0d70df3c7527eba5
Nattaphurin Vichit <58160565@go.buu.ac.th>
2017-12-17 19:29:13 +0700

    commit contact file

---
+++ contacts/index.js
@@ -0,0 +1,50 @@
+var lists = [
+    {
+        "ID" : "0",
+        "Firstname": "Piyapong",
+        "Lastname": "Paikamnam",
+        "URL": "https://www.facebook.com/piyaphong.paikamnam",
+        "Phone" : "082-5237431",
+        "Note" : ""
+    },
+    {
+        "ID" : "1",
+        "Firstname": "Nattaphurin",
+        "Lastname": "Vichit",
+        "URL": "https://www.facebook.com/profile.php?id=100003002294477",
+        "Phone" : "098-9367670",
+        "Note" : ""
+    },
+    {
+        "ID" : "2",
+        "Firstname": "NongMarch",
+        "Lastname": "songpunsibpad",
+        "URL": "https://www.facebook.com/MPongsapak",
+        "Phone" : "",
+        "Note" : ""
+    },
+    {
+        "ID" : "3",
+        "Firstname": "NongAut",
+        "Lastname": "Widwew",
+        "URL": "https://www.facebook.com/akkarapol.suasat",
+        "Phone" : "",
+        "Note" : ""
+    },
+    {
+        "ID" : "4",
+        "Firstname": "NongBut",
+        "Lastname": "some body help me",
+        "URL": "https://www.facebook.com/Bterpchy",
+        "Phone" : "",
+        "Note" : ""
+    }
+    ];
+    exports.findAllofList = function() {
+        return lites;
+    };
+    exports.findByFirstname = function (Firstname) {
+        for (var i = 0; i < users.length; i++) {
+            if (lists[i].Firstname ==Firstname) return lists[i];
+        }
+    };
